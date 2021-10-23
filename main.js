8398078c4abd70f5174e1f4f65bd9fdc12e16d81
jin1125 <shogo.jinta@gmail.com>
2021-10-23 18:28:37 +0900

    20. Stringオブジェクト その1 concat

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-function test() {
-  console.log("test");
-}
-
-// test();
-
-const comment = "comment";
-
-function getComment(string) {
-  console.log(string);
-}
-
-// getComment(comment);
-
-function getNumber() {
-  return 5;
-}
-
-// console.log(getNumber())
-
-function sumPrice(int1, int2) {
-  let int3 = int1 + int2;
-  return int3;
-}
-
-const total = sumPrice(3, 5);
-
-console.log(total);
+ const test =  'test';
+ const test_2 = 'desu'
+ console.log(test.concat(test_2));
