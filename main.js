e6d6629545adc18e219386b49e00f5b3b83e2a2a
jin1125 <shogo.jinta@gmail.com>
2021-10-23 19:39:27 +0900

    24. Arrayとアロー関数 foreach

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-const fruits = [
-  'apple','banana'
-]
-fruits.push('orange')
+const fruits = ["apple", "banana"];
+fruits.push("orange");
 
 // console.log(fruits);
 
-fruits.forEach((input)=>{
-  console.log(input);
-})
+fruits.forEach(input => console.log(input));
+
+// function getItem(){}
+
+const getItem = () => console.log("arrow");
+getItem();
