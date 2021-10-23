ea60f9871a12503cf2002db11939bc09b9519192
jin1125 <shogo.jinta@gmail.com>
2021-10-23 17:33:19 +0900

    JavaScriptのif文 その1

--- main.js
+++ main.js
@@ -3,4 +3,10 @@ let test_2 = 3;
 test_2 += 3;
 const test_3 = test_1 % test_2;
 
-console.log(test_3);
+// console.log(test_3);
+
+const height = 90;
+
+if(height === 90){
+  console.log('height90');
+}
