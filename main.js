5681d44800f58be26c56b8d1736f14864b4f21d7
jin1125 <shogo.jinta@gmail.com>
2021-10-23 17:39:41 +0900

    JavaScriptのif文 その2

--- main.js
+++ main.js
@@ -5,8 +5,27 @@ const test_3 = test_1 % test_2;
 
 // console.log(test_3);
 
-const height = 90;
+const height = 91;
 
-if(height === 90){
-  console.log('height90');
+// if(height === 90){
+//   console.log('height90');
+// }else{
+//   console.log('none');
+// }
+
+const signal = 'blue';
+
+if(signal === 'red'){
+  console.log('stop');
+}else if(signal === 'yellow'){
+  console.log('one stop');
+}else{
+  console.log('OK');
+}
+
+const speed =60;
+if(signal === 'blue'){
+  if(speed >= 60){
+    console.log('out');
+  }
 }
