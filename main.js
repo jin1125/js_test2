393263f11e9a7580d8728d2653beb7492247e74b
jin1125 <shogo.jinta@gmail.com>
2021-10-23 19:50:22 +0900

    25. Arrayとアロー関数 filter, find, map

--- main.js
+++ main.js
@@ -9,3 +9,27 @@ fruits.forEach(input => console.log(input));
 
 const getItem = () => console.log("arrow");
 getItem();
+
+
+const scores = [10,20,30,40];
+const newScores = scores.filter((value)=>{
+  return value >=30;
+})
+
+console.log(newScores);
+
+const members = ['honda','kagawa','nagatomo']
+
+const member = members.find((value)=>{
+ return value === 'nagatomo' 
+})
+
+console.log(member);
+
+const userList = [10,20,30,40]
+const userIdList = userList.map((value)=>{
+  return `user_${value}`
+})
+
+console.table(userIdList);
+
