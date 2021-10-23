1eb9e84be717445dbfff4c07e91caf6db4e68e42
jin1125 <shogo.jinta@gmail.com>
2021-10-23 19:55:13 +0900

    26. Objectオブジェクト(プロパティ)

--- main.js
+++ main.js
@@ -1,35 +1,11 @@
-const fruits = ["apple", "banana"];
-fruits.push("orange");
+const test = {
+  test1:10,
+  test2:20,
+  test3:30
+}
 
-// console.log(fruits);
+console.log(Object.values(test));
 
-fruits.forEach(input => console.log(input));
-
-// function getItem(){}
-
-const getItem = () => console.log("arrow");
-getItem();
-
-
-const scores = [10,20,30,40];
-const newScores = scores.filter((value)=>{
-  return value >=30;
-})
-
-console.log(newScores);
-
-const members = ['honda','kagawa','nagatomo']
-
-const member = members.find((value)=>{
- return value === 'nagatomo' 
-})
-
-console.log(member);
-
-const userList = [10,20,30,40]
-const userIdList = userList.map((value)=>{
-  return `user_${value}`
-})
-
-console.table(userIdList);
-
+for( value of Object.values(test)){
+  console.log(value);
+}
