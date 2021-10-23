15854db6a5eacb9669a57d05841cb3f0943e9204
jin1125 <shogo.jinta@gmail.com>
2021-10-23 17:29:11 +0900

    JavaScriptの演算子

--- main.js
+++ main.js
@@ -1,49 +1,6 @@
-const array =[1,2,3];
+const test_1 = 4;
+let test_2 = 3;
+test_2 += 3;
+const test_3 = test_1 % test_2;
 
-const array_2 =[
-  ['aka','ao','kiro'],
-  ['midori','murasaki','kuro']
-];
-
-// console.log(array_2[1][2]);
-
-const member = {
-  name:'honda',
-  height:170,
-  hobby:'soccer'
-}
-
-console.log(member.name);
-
-const mumber_2 ={
-  honda:{
-    height:170,
-    hobby:'soccer'
-  },
-  kagawa:{
-    height:165,
-    hobby:'soccer'
-  }
-}
-
-// console.log(mumber_2.kagawa.height);
-
-const mumber_3 ={
- "1kumi":{
-  honda:{
-    height:170,
-    hobby:'soccer'
-  },
-  kagawa:{
-    height:165,
-    hobby:'soccer'
-  }
-
-  },
- " 2kumi":{
-    
-  }
-}
-
-console.log(mumber_3);
-
+console.log(test_3);
