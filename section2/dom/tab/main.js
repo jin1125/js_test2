aeedc029822ce0e428203942a32d56f7263a0b29
jin1125 <shogo.jinta@gmail.com>
2021-10-24 16:59:30 +0900

    43. サンプル2：タブメニュー

--- section2/dom/tab/main.js
+++ section2/dom/tab/main.js
@@ -1,27 +1,26 @@
-'use strict';
+"use strict";
 
 {
-const tabLabels = document.querySelectorAll('.tab__label li a');
-const tabContents = document.querySelectorAll('.tab__content');
+  const tabLabels = document.querySelectorAll(".tab__label li a");
+  const tabContents = document.querySelectorAll(".tab__content");
 
-// forEach for-of
-tabLabels.forEach( clickedLabel => {
-  clickedLabel.addEventListener('click', e => {
-    e.preventDefault();
+  // forEach for-of
+  tabLabels.forEach((clickedLabel) => {
+    clickedLabel.addEventListener("click", (e) => {
+      console.log(clickedLabel);
+      e.preventDefault();
 
-    tabLabels.forEach(label => {
-      label.classList.remove('active');
+      tabLabels.forEach((label) => {
+        label.classList.remove("active");
+      });
+
+      clickedLabel.classList.add("active");
+
+      tabContents.forEach((content) => {
+        content.classList.remove("active");
+      });
+
+      document.getElementById(clickedLabel.dataset.id).classList.add("active");
     });
-    
-    clickedLabel.classList.add('active');
-
-    tabContents.forEach(content => {
-      content.classList.remove('active');
-    });
-
-  document.getElementById(clickedLabel.dataset.id).classList.add('active');
-
-  })
-});
-
+  });
 }
