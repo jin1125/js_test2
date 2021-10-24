af0a26f2fba1618b9533720c89c29f377b315c73
jin1125 <shogo.jinta@gmail.com>
2021-10-24 15:53:09 +0900

    41. 2-2. イベントオブジェクトとバブリング

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const target = document.getElementById('target')
-target.addEventListener('click',()=>{
+target.addEventListener('click',(e)=>{
+  console.log(e);
   const test = document.getElementById('test');
   test.textContent = 'test'
   test.classList.add('red');
