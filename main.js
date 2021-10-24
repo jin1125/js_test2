d80e8e66914cd0d35a248af467aec05f9c8c3b15
jin1125 <shogo.jinta@gmail.com>
2021-10-24 13:20:58 +0900

    38. 3−2. 要素や属性のつくり方・つけ方

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-const test = document.getElementById('test');
-test.classList.add('red');
+// const test = document.getElementById('test');
+// test.classList.add('red');
 
-const testList = document.querySelector('.test_list');
-testList.classList.remove('blue');
+// const testList = document.querySelector('.test_list');
+// testList.classList.remove('blue');
 
-console.log(test);
-console.log(testList);
+// console.log(test);
+// console.log(testList);
+
+const input = document.getElementById('test');
+input.type="text" 
+input.placeholder="aaa"
+input.name='aaa'
+input.textContent = 'te'
+
+const anchor = document.createElement('a');
+anchor.href = 'https://google.com'
+anchor.target = "_blank"
+
+console.log(input);
+console.log(anchor);
