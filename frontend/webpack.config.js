c8c9ec4673cd69e28bdf32970adb2091df16ac4d
jin1125 <shogo.jinta@gmail.com>
2021-10-25 23:18:37 +0900

    86. CSS-loaderのインストール・設定

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,4 +6,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [{ test: /\.css$/, use: ['style-loader','css-loader'] }],
+  },
 };
