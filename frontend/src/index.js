fbd3483f93f5f63c719fa1fe7583af5ab9494a03
jin1125 <shogo.jinta@gmail.com>
2021-10-25 23:52:59 +0900

    88. babelのインストール・設定

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,2 +1,6 @@
 import './style.scss'
 console.log('webpack');
+
+window.addEventListener('load',()=>{
+  alert('babel')
+})
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,12 +1,27 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
-    rules: [{ test: /\.(sass|scss)$/, use: ['style-loader','css-loader','sass-loader'] }],
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
   },
 };
